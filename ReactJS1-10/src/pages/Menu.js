bee5d862c24ef84ac39ca724cdd3b37e81ec1a41
Alfian Reno Prasetyo <70306653+AlfianReno@users.noreply.github.com>
2022-02-09 16:48:31 +0700

    Add files via upload

---
+++ ReactJS1-10/src/pages/Menu.js
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+import Tabel from './Tabel';
+function Menu() {
+    const titel = "Daftar Menu Restoran";
+    const [menus,setMenu] = useState(
+        [
+            {
+                idmenu:15, 
+                idkategori:56, 
+                menu:"Es Jeruk", 
+                gambar:"esjeruk.jpg", 
+                harga:6000,
+            },
+            {
+                idmenu:23, 
+                idkategori:56, 
+                menu:"Es Teh", 
+                gambar:"esteh.jpg", 
+                harga:3000,
+            },
+            {
+                idmenu:27, 
+                idkategori:44, 
+                menu:"Apel Manalagi", 
+                gambar:"apel.jpg", 
+                harga:3000,
+            },
+            {
+                idmenu:28, 
+                idkategori:44, 
+                menu:"Pisang Raja", 
+                gambar:"pisang.jpg", 
+                harga:5000,
+            },
+            {
+                idmenu:29, 
+                idkategori:41, 
+                menu:"Nasi Padang", 
+                gambar:"nasipadang.jpg", 
+                harga:20000,
+            },
+            {
+                idmenu:30, 
+                idkategori:41, 
+                menu:"Nasi Ayam", 
+                gambar:"nasiayam.jpg", 
+                harga:15000,
+            }
+        ]
+    );
+  return (
+    <div className="App">
+      <Tabel menu={menus} titel={titel}/>
+    </div>
+    
+  );
+}
+
+export default Menu;
---
+++ ReactJS1-10/src/pages/Sejarah.js
@@ -0,0 +1,12 @@
+function Sejarah() {
+  return (
+    <div className="App">
+      <h1>Sejarah Alfian Reno</h1>
+      <p>Isi sejarah</p>
+      <img src="images/logo.png" alt="logo" />
+    </div>
+    
+  );
+}
+
+export default Sejarah;
