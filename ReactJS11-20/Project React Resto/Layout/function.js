d3ac70d25c8234a128d487057c9f06f457c52ed1
Alfian Reno Prasetyo <70306653+AlfianReno@users.noreply.github.com>
2022-02-17 15:06:46 +0700

    Add files via upload

---
+++ ReactJS11-20/Project React Resto/Layout/function.js
@@ -0,0 +1,18 @@
+export function get () {
+    console.log("get");
+}
+
+export function show () {
+    console.log("show");
+}
+
+export const siswa = [
+    {
+        nama : 'siti',
+        kelas : '11 rpl'
+    },
+    {
+        nama : 'budi',
+        kelas : '12 rpl'
+    }
+];
---
+++ ReactJS11-20/Project React Resto/Layout/link.js
@@ -0,0 +1,9 @@
+const url = "http://localhost:8000/api";
+let token = "bWLhS3atZck5ent0aTdPjzmdnF0tUdQRL3ZNMSuk";
+
+export const link = axios.create({
+    baseURL: url,
+    headers: {
+        'api_token': token
+    }
+});
