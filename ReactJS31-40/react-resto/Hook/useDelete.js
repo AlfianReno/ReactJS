cc23b5f3b86f7b978a26a41c7f6ff626aad3b89f
Alfian Reno Prasetyo <70306653+AlfianReno@users.noreply.github.com>
2022-03-03 17:08:08 +0700

    Add files via upload

---
+++ ReactJS31-40/react-resto/Hook/useDelete.js
@@ -0,0 +1,15 @@
+import { link } from "../Axios/link";
+import { useState } from "react";
+const useDelete = (url) => {
+  const [pesan, setPesan] = useState("");
+
+  async function hapus(id) {
+    if (window.confirm("Yakin akan dihapus?")) {
+      const res = await link.delete(url + id);
+      setPesan(res.data.pesan);
+    }
+  }
+  return { hapus, pesan, setPesan };
+};
+
+export default useDelete;
