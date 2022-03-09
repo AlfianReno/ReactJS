e8c0dd972bf8ac818e0e18696539a843ad10c94f
Alfian Reno Prasetyo <70306653+AlfianReno@users.noreply.github.com>
2022-03-09 17:07:59 +0700

    Add files via upload

---
+++ ReactJS41-50/Lumen/web.php
@@ -0,0 +1,64 @@
+<?php
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+$router->post('api/register' , ['uses' => 'LoginController@register']);
+$router->post('api/login' , ['uses' => 'LoginController@login']);
+
+//index
+$router->get('api/kategori' , ['uses' => 'KategoriController@index']);
+$router->get('api/menu' , ['uses' => 'MenuController@index']);
+$router->get('api/pelanggan' , ['uses' => 'PelangganController@index']);
+$router->get('api/order' , ['uses' => 'OrderController@index']);
+$router->get('api/user' , ['uses' => 'LoginController@index']);
+
+// create
+$router->post('api/kategori' , ['uses' => 'KategoriController@create']);
+$router->post('api/menu' , ['uses' => 'MenuController@create']);
+
+// delete
+$router->delete('api/pelanggan/{id}' , ['uses' => 'PelangganController@destroy']);
+$router->delete('api/menu/{id}' , ['uses' => 'MenuController@destroy']);
+
+// show id
+$router->get('api/menu/{id}' , ['uses' => 'MenuController@show']);
+$router->get('api/order/{a}/{b}' , ['uses' => 'OrderController@show']);
+$router->get('api/detail/{a}/{b}' , ['uses' => 'DetailController@show']);
+
+//update
+$router->post('api/menu/{id}' , ['uses' => 'MenuController@update']);
+$router->put('api/order/{id}' , ['uses' => 'OrderController@update']);
+
+
+$router->group(['prefix' => 'api','middleware'=>'auth'] , function() use ($router) {
+    
+    $router->get('kategori/{id}' , ['uses' => 'KategoriController@show']);
+    $router->get('pelanggan/{id}' , ['uses' => 'PelangganController@show']);
+
+    $router->delete('kategori/{id}' , ['uses' => 'KategoriController@destroy']);
+    
+
+    $router->put('kategori/{id}' , ['uses' => 'KategoriController@update']);
+    $router->put('pelanggan/{id}' , ['uses' => 'PelangganController@update']);
+
+    
+    $router->post('pelanggan' , ['uses' => 'PelangganController@create']);
+    
+});
+
+
