6dbdd2227083db98b5d8da7d84d402984f474129
Alfian Reno Prasetyo <70306653+AlfianReno@users.noreply.github.com>
2022-03-09 17:06:17 +0700

    Add files via upload

---
+++ ReactJS41-50/Lumen/Controller/LoginController.php
@@ -0,0 +1,58 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class LoginController extends Controller
+{
+    public function index()
+    {
+        $data = User::where('level','<>','pelanggan')->get();
+        return response()->json($data);
+    }
+
+    public function register(Request $request) {
+        $data = [
+            'email' => $request->input('email'),
+            'password' => $request->input('password'),
+            'level' => $request->input('level'),
+            'api_token' => '123456',
+            'status' => '1',
+            'relasi' => $request->input('relasi')
+        ];
+
+        User::create($data);
+
+        return response()->json($data);
+    }
+
+    public function login(Request $request) {
+        $email = $request->input('email');
+        $password = $request->input('password');
+
+        $user = User::where('email',$email)->first();
+
+        if($user->password === $password) {
+            $token = Str::random(40);
+
+            $user->update([
+                'api_token' => $token
+            ]);
+
+            return response()->json([
+                'pesan' => 'LOGIN BERHASIL',
+                'token' => $token,
+                'data' => $user
+            ]);
+        }else {
+            return response()->json([
+                'pesan' => 'LOGIN GAGAL',
+                'data' => ''
+            ]);
+        }
+    }
+}
